4442db8c50dd1b88172ed2e113f480b8c30cba33
wasim152 <wasim.abbas@ymail.com>
2023-07-29 19:52:02 +0100

    tag

--- main.tf
+++ main.tf
@@ -33,7 +33,10 @@ resource "aws_instance" "web" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-
+  tags {
+        "key": "value",
+        "Name": "Value"
+  } 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
